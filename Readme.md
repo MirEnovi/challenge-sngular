3c54001890ac29642991668b80b5dfd215a2f61c
MirEnovi <garciaivonne11@gmail.com>
2018-11-28 01:18:36 -0600

    add new styles

--- Readme.md
+++ Readme.md
@@ -40,4 +40,4 @@ Para este proyecto se uso:
 
 ## Versión
 
-### Release V3.0.0
+### Release V3.1.0
--- app.js
+++ app.js
@@ -5,18 +5,34 @@ const btnStyle = document.getElementById('btn-style');
 const btnTryAgaint = document.getElementById('btn-try-againt');
 const styleInput = document.getElementById('style-input');
 const styleP = document.getElementById('style-p');
+let count = 0;
+
 
 const putTextStyle = () => {
   const textEdit=textInput.value;
   styleInput.style.display='none';
   styleP.style.display='block';
-  textStyle.setAttribute('class', 'style-p col-10 offset-1 justify-content-center text-center')
+  count ++;
+  console.log(count);
+  if(count===1){
+    textStyle.setAttribute('class', 'style-a col-10 offset-1 justify-content-center text-center');
+  } else if (count===2) {
+    textStyle.setAttribute('class', 'style-b col-10 offset-1 justify-content-center text-center');
+  } else if (count===3) {
+    textStyle.setAttribute('class', 'style-c col-10 offset-1 justify-content-center text-center');
+  } else {
+    count = 0;
+    putTextStyle();
+  }
+
 
   textStyle.innerHTML=textEdit;
 };
 
 const reload = () => {
-  window.location.reload();
+  // window.location.reload();
+  styleInput.style.display='block';
+  styleP.style.display='none';
 }
 
 btnStyle.addEventListener('click', putTextStyle);
